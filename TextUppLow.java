41b0252c60377e6c1a5e810ad82cd94ccb065f74
rushikeshjanorkar <118961355+rushikeshjanorkar@users.noreply.github.com>
2023-01-25 10:50:34 +0530

    Update TextUppLow.java

--- TextUppLow.java
+++ TextUppLow.java
@@ -3,11 +3,47 @@
 
 package JavaPractice;
 
-public class TextUppLow {
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI.MouseHandler;
 
+import java.awt.*;
+import java.awt.event.*;
+
+public class TextUppLow extends  JFrame {
+
+private JTextField txt1 ;
+	
+	public  TextUppLow() {
+		txt1 = new  JTextField(50)  ;
+		
+		setTitle("Mouse Click");
+		setSize(300, 400);
+		setLocation(300,300);
+		setLayout(new FlowLayout());
+		add(txt1);
+		setVisible(true);
+		
+		MouseHandler mh= new  MouseHandler();
+		txt1.addMouseListener(mh);
+		}
+	
+ class MouseHandler extends MouseAdapter{
+		public void mouseClicked(MouseEvent e) {
+			String s= txt1.getText();
+			if(e.getButton()==MouseEvent.BUTTON1) {
+				txt1.setText(s.toUpperCase());
+			}
+			if(e.getButton()==MouseEvent.BUTTON3) {
+				txt1.setText(s.toLowerCase());
+			}
+		}
+ }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new TextUppLow();
+		
+		
+		
 	}
 
 }
