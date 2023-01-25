21102fd6c9215c3e3af4220ed41815921b788edc
rushikeshjanorkar <118961355+rushikeshjanorkar@users.noreply.github.com>
2023-01-25 10:54:52 +0530

    Update BirthDateExcp.java

--- BirthDateExcp.java
+++ BirthDateExcp.java
@@ -4,71 +4,84 @@ date, month and year. Raise an exception if the birthdate is invalid.*/
 package JavaPractice;
 
 
+class Invalid extends Exception{
+	
+}
 public class BirthDateExcp {
-int day,month,year;
-boolean state,checkmonth,checkday;
+      private int date,month,year;
+      
+      public BirthDate(int d,int m,int y) {
+    	  date=d;
+    	  month=m;
+    	  year=y;
+    	  
+      }
+      public void Display() {
+    	  System.out.println(date +"/"+month+"/"+year);
+      }
 	
-	BirthDateExcp(int d,int m ,int y)
-	{
-		day=d;
-		month=m;
-		year=y;
-	}
-	boolean checkmonth()
-	{
-		if(month <1 || month >12 )
-			return false;
+      
+      public static void main(String[] args) {
 		
-		else return true;
-	}
-	
-	boolean checkday()
-	{
-		if(day <1 || day >31)
-			state=false;
+		int d,m,y;
+		d=Integer.parseInt(args[0]);
+		m=Integer.parseInt(args[1]);
+		y=Integer.parseInt(args[2]);
 		
-		else {
-			switch(month)
-			{
-			case 1:
-				
-			case 3:
-				
-			case 5:
-			case 7:
-				
-			case 8:
-			case 10:
-				
-			case 12:
-				if(day >31)
-				{
-					state =false;
-				}
-				else 
-					state =true;
-				break;
-				
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if(day >30)
-				{
-					
-				}
-
-			}
-				
-		}
-		return checkday;
-	}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
+      try {
+    	  switch(m)
+    		{
+    			case 4:
+    		    case 6:
+    	     	case 9:
+    			case 11:
+    				if (d>=1 && d<=30)
+    				{}
+    				else throw   new Invalid ();
+    				break ;
+    				
+    				
+    			case 1:
+    			case 3:
+    			case 5:
+    			case 7:
+    			case 8:
+    			case 10:
+    			case 12:
+    			   	if (d>=1 && d<=31)
+    			   	{}
+    				else throw   new Invalid ();
+    				break ;
+    			    	
+    			case 2:
+    				if ((y%4==0 && y%100!=0 || y%400==0))
+    			    	{
+    			    		if(d>=1 && d<=29)
+    			    		{}
+    	    				else throw   new Invalid ();
+    	    				break ;
+    					}
+    					else
+    					{
+    						if(d>=1 && d<=28)
+    						{}
+    	    				else throw   new Invalid ();
+    	    				break ;
+    			    			
+    					}	
+    			default :
+    		
+				throw   new Invalid ();
+			
+    		}
+    	  BirthDateExcp bd = new BirthDateExcp(d, m, y);
+    	  
+    	  bd.Display();
+      }catch (Invalid e) {
+    	  System.out.println("Invalid Brith date");
+      }
+      
 	}
 
 }
